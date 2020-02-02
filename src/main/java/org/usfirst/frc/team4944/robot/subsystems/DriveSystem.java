541b6523d4d0fa344cb322eb8cb641fa813d781b
JacksonKull <35639675+JacksonKull@users.noreply.github.com>
2020-02-02 14:55:13 -0700

    bot prep

--- src/main/java/org/usfirst/frc/team4944/robot/subsystems/DriveSystem.java
+++ src/main/java/org/usfirst/frc/team4944/robot/subsystems/DriveSystem.java
@@ -9,7 +9,7 @@ import com.kauailabs.navx.frc.AHRS;
 import edu.wpi.first.wpilibj.SerialPort.Port;
 
 public class DriveSystem {
-	//NOAH is a poopy head
+	//Cus your a loser
 	// MOTORS
 	TalonSRX leftMotor1;
 	TalonSRX leftMotor2;
